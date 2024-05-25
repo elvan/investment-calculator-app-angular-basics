ff7b386d5553b4b750629dcc90b53d4d60afc890
Elvan Hidayat <elvanhd@gmail.com>
2024-05-25 21:10:41 +0700

    Refactor to use NgModule for component organization
    - Create AppModule to declare and import components
    - Move UserInputComponent into UserInputModule
    - Update main.ts to bootstrap AppModule instead of AppComponent
    - Remove standalone declarations from components
    - Import necessary modules in AppModule and UserInputModule

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
