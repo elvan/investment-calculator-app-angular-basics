2aa3f0aa4d6a034ac8b4e2aa23e35fd78b90c686
Elvan Hidayat <elvanhd@gmail.com>
2024-05-25 19:26:56 +0700

    Add InvestmentResults component to display results
    - Update app.component.html to include InvestmentResults component
    - Modify AppComponent to store and pass calculation results
    - Create investment-results.component.ts for displaying results
    - Add investment-results.component.html for table structure
    - Add investment-results.component.css for table styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
@@ -33,6 +43,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
