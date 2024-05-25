585bf2b46d07624970b026a754092fc1b618f9a8
Elvan Hidayat <elvanhd@gmail.com>
2024-05-25 18:45:15 +0700

    Enhance UserInput component with ngModel bindings
    - Add ngModel bindings to form inputs in user-input.component.html
    - Initialize form fields in user-input.component.ts
    - Update onSubmit method to log the form field values

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -9,7 +9,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
   onSubmit() {
     console.log('SUBMITTED!');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 }
